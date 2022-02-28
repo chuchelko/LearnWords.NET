093206096f8d541a1c63df22eb2179a3a473a2bd
Emil <emilnizamov9@gmail.com>
2022-02-28 19:25:01 +0300

    Commit

--- LearnWords.WebApp/Pages/Index.cshtml.cs
+++ LearnWords.WebApp/Pages/Index.cshtml.cs
@@ -12,43 +12,33 @@ public class IndexModel : PageModel
     private readonly ILogger<IndexModel> _logger;
     private readonly IMediator _mediator;
     public UserDTO _user;
-    public List<WordDTO> _words;
     private readonly IMapper _mapper;
-    public DAL.Entities.Language FirstLang { get; set; }
-    public DAL.Entities.Language SecondLang { get; set; }
 
 
     public IndexModel(ILogger<IndexModel> logger, IMediator mediator, IMapper mapper)
     {
-        FirstLang = DAL.Entities.Language.English;
         _logger = logger;
         _mediator = mediator;
         _mapper = mapper;
         Task<UserDTO> userTask = _mediator.Send(new GetUser.Query(Guid.Empty));
         userTask.Wait();
         _user = userTask.Result;
+    }
+    public JsonResult OnGetFetchWords(DAL.Entities.Language FirstLang, DAL.Entities.Language SecondLang)
+    {
         Task<List<WordDTO>> wordsTask = _mediator.Send(new GetUsersWords.Query(_user.Id, FirstLang, SecondLang));
         wordsTask.Wait();
-        _words = wordsTask.Result;
-    }
-    public JsonResult OnGetFetchWords(int FirstLang, int SecondLang)
-    {
-        this.FirstLang = (DAL.Entities.Language)FirstLang;
-        this.SecondLang = (DAL.Entities.Language)SecondLang;
-        Task<List<WordDTO>> wordsTask = _mediator.Send(new GetUsersWords.Query(_user.Id, this.FirstLang, this.SecondLang));
-        wordsTask.Wait();
-        _words = wordsTask.Result;
+        List<WordDTO> _words = wordsTask.Result;
 
         foreach (var word in _words)
-            if (word.From != this.FirstLang)
+            if (word.From != FirstLang)
                 (word.WordTo, word.WordFrom) = (word.WordFrom, word.WordTo);
         _logger.LogInformation($"{FirstLang} {SecondLang}");
         return new JsonResult(_words);
     }
-    public async Task<IActionResult> OnPostAddWordsAsync(string FirstLangWord, string SecondLangWord, int FirstLang, int SecondLang)
+    public void OnGetAddWords(string FirstWord, string SecondWord, DAL.Entities.Language FirstLang, DAL.Entities.Language SecondLang)
     {
-        await _mediator.Send(new CreateWord.Command(_user.Id, (DAL.Entities.Language)FirstLang, (DAL.Entities.Language)SecondLang, FirstLangWord, SecondLangWord, null));
-        _logger.LogInformation($"Добавлен объект {FirstLang}-{SecondLang} : {FirstLangWord}, {SecondLangWord}");
-        return RedirectToPage("./Index", new { FirstLang = FirstLang, SecondLang = SecondLang });
+        _mediator.Send(new CreateWord.Command(_user.Id, FirstLang, SecondLang, FirstWord, SecondWord, null));
+        _logger.LogInformation($"Добавлен объект {FirstLang}-{SecondLang} : {FirstWord}, {SecondWord}");
     }
 }
